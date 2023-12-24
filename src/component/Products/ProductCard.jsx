101acf3df3791f50b0c8d6666f9f7c53cd31afe6
Labib44 <mdlabib162@gmail.com>
2023-12-24 12:38:55 +0600

    Data load from Database

--- src/component/Products/ProductCard.jsx
+++ src/component/Products/ProductCard.jsx
@@ -7,12 +7,12 @@ import { MdOutlineLabelImportant } from "react-icons/md";
 
 
 const ProductCard = ({ product }) => {
-    const { productName, price, color, img } = product
+    const { name, price, color, imageUrl } = product
     return (
         <div className="w-full relative group">
             <div className="max-w-80 max-h-80 relative overflow-y-hidden ">
                 <div>
-                    <img src={img} alt="" />
+                    <img src={imageUrl} alt="" />
                 </div>
                 <div className="absolute top-6 left-8">
                     {/* {props.badge && <Badge text="New" />} */}
@@ -54,7 +54,7 @@ const ProductCard = ({ product }) => {
             <div className="max-w-80 py-6 flex flex-col gap-1 border-[1px] border-t-0 px-4">
                 <div className="flex items-center justify-between font-titleFont">
                     <h2 className="text-lg text-primeColor font-bold">
-                        {productName}
+                        {name}
                     </h2>
                     <p className="text-[#767676] text-[14px]">${price}</p>
                 </div>
--- src/component/Products/Products.jsx
+++ src/component/Products/Products.jsx
@@ -1,63 +1,27 @@
 import ProductCard from "./ProductCard";
-import img1 from "../../assets/products/1.jpg"
-import img2 from "../../assets/products/2.jpg"
-import img3 from "../../assets/products/3.jpg"
-import img4 from "../../assets/products/5.jpg"
-import img5 from "../../assets/products/11.jpg"
-import img6 from "../../assets/products/7.jpg"
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Products = () => {
-    const products = [
-        {
-            _id: 991,
-            productName: "T-shirt",
-            img: img1,
-            price: 45,
-            color: "gray",
-        },
-        {
-            _id: 992,
-            productName: "Denim Pant",
-            img: img2,
-            price: 75,
-            color: "black",
-        },
-        {
-            _id: 993,
-            productName: "T-shirt",
-            img: img3,
-            price: 45,
-            color: "gray",
-        },
-        {
-            _id: 994,
-            productName: "T-shirt",
-            img: img4,
-            price: 45,
-            color: "gray",
-        },
-        {
-            _id: 995,
-            productName: "T-shirt",
-            img: img5,
-            price: 45,
-            color: "gray",
-        },
-        {
-            _id: 996,
-            productName: "T-shirt",
-            img: img6,
-            price: 45,
-            color: "gray",
-        },
-    ]
+    const [products, setProducts] = useState([]);
+
+    const getMyApi = async () => {
+        const res = await axios.get("http://localhost:5000/api/v1/product")
+        setProducts(res.data.data);
+    }
+
+    useEffect(() => {
+        getMyApi()
+    }, [])
+    console.log("my data", products);
+
     return (
         <div className=' grid grid-cols-3 lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 gap-5'>
 
             {
-                products.map((product)=><ProductCard
-                key={product._id}
-                product={product}
+                products?.map((product) => <ProductCard
+                    key={product._id}
+                    product={product}
                 ></ProductCard>)
             }
         </div>
